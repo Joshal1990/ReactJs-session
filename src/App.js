88b42398fa7f3101308b1f80f46961051839f7cf
paul.jopal@gmail.com <johraphe@in.ibm.com>
2019-07-03 00:04:01 +0530

    added more file in react js tutorials

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Board from './Board';
 
 class App extends React.Component {
-  constructor(prop){
-    super(prop);
-    this.state = {
-      name: prop.name
-    }
-  }
+  
   render(){
   return (
-    <UserDetail></UserDetail>
+    // <div className="game">
+    //   <div className = "game-board">
+    //       <Board/>
+    //   </div>
+    //   <div className = "game-info">
+    //     <div>{/* status */}</div>
+    //     <div>{/* TODO */}</div>
+    //   </div>
+    // </div>
+    <Board/>
   );
   }
 }
